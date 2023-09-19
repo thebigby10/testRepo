4c1723a44b8a8870b88c4ff66dbbd93b63cb6995
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2023-09-19 22:53:26 +0600

    erothosis

--- prime.c
+++ prime.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
+#include<math.h>
 
+int PRIME[1000] = {};
 int isPrime(int x){
-    if(x==1) return 0;
-    for(int i=2;i<x;i++) if(x%i==0) return 0;
-    return 1;
+    PRIME[1] = 0;
+    PRIME[2] = 1;
+    for(int i=2;i<x;i++){
+        
+    }
 }
 
+
 int main(){
-    //for(int i=1;i<100;i++)
-    //    printf("%d = %d\n",i,isPrime(i));
+
+    for(int i=1;i<100;i++)
+        printf("%d = %d\n",i,isPrime(i));
     return 0;
 }
